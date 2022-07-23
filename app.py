f2d9ab80c2c95f5339608aa0ccd9d43afa2a5990
Billy Cao <aliencaocao@gmail.com>
2022-07-23 12:18:20 +0800

    Add functions for stocks and leaders vote records

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def connect_db(user='root', password='root', host='127.0.0.1', port=3306, databa
     except Exception as e:
         raise Exception(f'Error connecting to MariaDB: {e}')
     else:
-        print(f'Connected to MariaDB {database}')
+        # print(f'Connected to MariaDB {database}')
         db.autocommit = autocommit
         return db
 
@@ -23,7 +23,7 @@ def close_db(cursor, db):
     db.close()
 
 
-def register(username, password):
+def register(username, password):  # does check for duplicate username
     password = hashlib.sha512(password.encode()).hexdigest()
     db = connect_db()
     cursor = db.cursor()
@@ -58,7 +58,7 @@ def login(username, password):
         return bool(user)
 
 
-def get_username(userid):
+def get_username(userid):  # userid can be invalid
     db = connect_db()
     cursor = db.cursor()
     try:
@@ -72,17 +72,23 @@ def get_username(userid):
         return username[0] if username else f'Error: user ID {userid} not found'
 
 
-def vote_leader(userid):  # assumes userid is valid and is not self, can use get_username() to check existence
+def vote_leader(candidate, voter, up=True):  # assumes userid is valid and is not self, assumes voter has not previously voted for this candidate
     db = connect_db()
     cursor = db.cursor()
     try:
-        cursor.execute(f'UPDATE users SET leader_votes = leader_votes + 1, register_time = register_time WHERE id = ?', (userid,))
+        cursor.execute(f'INSERT INTO leader_votes (candidate, voter, direction) VALUES (?, ?, ?)', (candidate, voter, 1 if up else -1))
     except Exception as e:
         close_db(cursor, db)
         return f'Error: {e}'
     else:
-        close_db(cursor, db)
-        return 'Voted successfully!'
+        try:
+            cursor.execute(f'UPDATE users SET leader_votes = leader_votes {"+" if up else "-"} 1, register_time = register_time WHERE id = ?', (candidate,))
+        except Exception as e:
+            close_db(cursor, db)
+            return f'Error: {e}'
+        else:
+            close_db(cursor, db)
+            return 'Voted successfully!'
 
 
 def toggle_leader(userid):  # assumes userid is valid
@@ -105,12 +111,27 @@ def add_stock(ticker, description, userid):  # assumes ticker is not added yet, 
     cursor = db.cursor()
     try:
         cursor.execute(f'INSERT INTO stocks (ticker, description, posted_by) VALUES (?, ?, ?)', (ticker, description, userid))
+        vote_stock(ticker, userid)
     except Exception as e:
         close_db(cursor, db)
         return f'Error: {e}'
     else:
         close_db(cursor, db)
         return f'Added {ticker} successfully!'
+
+
+def check_stock_exists(ticker):
+    db = connect_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute(f'SELECT ticker FROM stocks WHERE ticker = ?', (ticker,))
+        stock = cursor.fetchone()
+    except Exception as e:
+        close_db(cursor, db)
+        return f'Error: {e}'
+    else:
+        close_db(cursor, db)
+        return bool(stock)
 
 
 def get_stocks():
@@ -128,21 +149,58 @@ def get_stocks():
         return stocks
 
 
-def vote_stock(ticker, up=True):  # assumes ticker is valid
+def vote_stock(ticker, voter, up=True):  # assumes ticker and voter is valid, assumes voter has not previously voted for this ticker
     db = connect_db()
     cursor = db.cursor()
     try:
-        cursor.execute(f'UPDATE stocks SET votes = votes {"+" if up else "-"} 1 WHERE ticker = ?', (ticker,))
+        cursor.execute(f'INSERT INTO stock_votes (ticker, voter, direction) VALUES (?, ?, ?)', (ticker, voter, 1 if up else -1))
     except Exception as e:
         close_db(cursor, db)
         return f'Error: {e}'
     else:
+        try:
+            cursor.execute(f'UPDATE stocks SET votes = votes {"+" if up else "-"} 1 WHERE ticker = ?', (ticker,))
+        except Exception as e:
+            close_db(cursor, db)
+            return f'Error: {e}'
+        else:
+            close_db(cursor, db)
+            return f'Voted {"up" if up else "down"} {ticker} successfully!'
+
+
+def check_leader_voted(candidate, voter):  # assumes both userid is valid, checks if voter has voted for the candidate
+    db = connect_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute(f'SELECT candidate, voter, direction FROM leader_votes WHERE candidate = ? AND voter = ?', (candidate, voter))
+        votes = cursor.fetchone()
+    except Exception as e:
         close_db(cursor, db)
-        return f'Voted {"up" if up else "down"} {ticker} successfully!'
+        return f'Error: {e}'
+    else:
+        if votes:
+            return votes[2]  # returns 1 or -1
+        else:
+            return False
+
+
+def check_stock_voted(ticker, voter):  # assumes both userid and ticker is valid, checks if voter has voted for the ticker
+    db = connect_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute(f'SELECT ticker, voter, direction FROM stock_votes WHERE ticker = ? AND voter = ?', (ticker, voter))
+        votes = cursor.fetchone()
+    except Exception as e:
+        close_db(cursor, db)
+        return f'Error: {e}'
+    else:
+        if votes:
+            return votes[2]  # returns 1 or -1
+        else:
+            return False
 
 
 print(register('test', 'test'))
-print(vote_leader(1))
 print(get_stocks())
 
 
